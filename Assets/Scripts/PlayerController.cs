0625827f64b0f868537d3f8c552df3a28222f43b
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-26 21:35:39 +0300

    add

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _speed = 2.2f;
 
+    [SerializeField]
+    private float _jump = 4.1f;
+
+    [Header("collisions sector")]
+    [SerializeField]
+    private Transform groundCheckPoint;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    public bool isGrounded;
+    private bool canJump;
 
     Rigidbody2D rb2D;
     // Start is called before the first frame update
@@ -23,6 +35,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
+        Jump();
+    }
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if(c.gameObject.GetComponent<BoostUP>())
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, 0);
+            rb2D.AddForce(transform.up * 6.2f, ForceMode2D.Impulse);
+        }
     }
 
     private void Movement()
@@ -34,6 +56,17 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector2.right * -_speed * Time.deltaTime);
+        }
+    }
+
+    private void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            print("pressed");
+            rb2D.velocity = new Vector2(rb2D.velocity.x, 0);
+            rb2D.AddForce(transform.up * _jump, ForceMode2D.Impulse);
         }
     }
 }
